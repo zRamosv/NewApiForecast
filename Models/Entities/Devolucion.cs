ded3d6eadb7ee75a8838fcbcf429fcfb0b31d52d
Israel Ochoa <israOchoa2233@outlook.com>
2024-11-06 15:25:49 -0600

    Implements Devoluciones Controller

--- Models/Entities/Devolucion.cs
+++ Models/Entities/Devolucion.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ApiForecast.Models.Entities
 {
     public class Devolucion
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public int ID_Devolucion { get; set; }
+        [ForeignKey("Pedido_ID")]
         public int Pedido_ID { get; set; }
         public Ventas Pedido { get; set; }
+        [ForeignKey("ID_Cliente")]
         public int ID_Cliente { get; set; }
         public Clientes Cliente { get; set; }
         public decimal Importe { get; set; }
